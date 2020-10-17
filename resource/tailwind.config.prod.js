fb322d4ce02ca40b66c1eadcb24d54b801474b70
insua <insua@sohu.com>
2020-10-17 15:55:41 +0800

    feat(home frontend): add laravel mix and tailwind

---
+++ resource/tailwind.config.prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: [
+    "./template/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
--- resource/webpack.mix.js
+++ resource/webpack.mix.js
@@ -1,8 +1,19 @@
 let mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
 
 mix.setPublicPath('../public/')
 
-mix.js('js/app.js', 'js/app.js').sass('scss/app.scss', 'css/app.css')
+mix.js('js/app.js', 'js/app.js')
+
+let sassOption = mix.inProduction() ? {
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.prod.js') ],
+  } : {
+  processCssUrls: false,
+  postCss: [ tailwindcss('./tailwind.config.js') ],
+}
+
+mix.sass('scss/app.scss', 'css/app.css').options(sassOption)
 
 if (mix.inProduction()) {
   mix.version().disableNotifications()
